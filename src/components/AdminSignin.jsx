4f1f7be9cc4367646582d4ec62323eb67f75c4aa
Rahul Raj <rahulraaz3142@gmail.com>
2025-04-28 01:11:01 +0530

    navbar env

--- src/components/AdminSignin.jsx
+++ src/components/AdminSignin.jsx
@@ -3,7 +3,9 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import "../Styles/AdminSignin.css";
 import { Navbar } from './Navbar';
 
-const URL = "https://students-way-b.onrender.com/api/auth/login";
+// const URL = "https://students-way-b.onrender.com/api/auth/login";
+const URL = import.meta.env.VITE_LOGIN_API;
+
 
 export default function AdminSignin() {
 

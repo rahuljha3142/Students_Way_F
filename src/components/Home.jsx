42196a65c74c7f99b9391258e649a7d2031fd1d8
Rahul Raj <rahulraaz3142@gmail.com>
2025-04-30 18:44:08 +0530

    navbar fix2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
                 <p className="TextContainer">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas molestias libero ipsum sit consequuntur illo ut praesentium officiis amet delectus commodi quos dolore sapiente fugit quaerat hic architecto, ab natus porro. Temporibus, nam distinctio.
                 </p>
-                    <NavLink className="HomeLink" to="/admin-register">Admin Register</NavLink>
+                    {/* <NavLink className="HomeLink" to="/admin-register">Admin Register</NavLink> */}
             </div>
             <img className='SchoolImage' src={bg} alt="pupil" />
         </div>

bcb3c6400b63190e1e0a890c0d9a8025b09b7213
Rahul Raj <rahulraaz3142@gmail.com>
2025-05-13 12:01:55 +0530

    storeTokenInLS2

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
 // import './index.css'
+import { AuthProvider } from './store/Auth.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </AuthProvider>
 )
